db567f1f1d06c144b00becfcee96fd1fd90dc0cb
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-25 17:09:01 +0800

    Delete Floral directory

--- Floral/Floral/Classes/Tools/3rdLib/Floral-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "MBProgressHUD.h"
-#import "UMSocial.h"
-#import "UMSocialSinaSSOHandler.h"
-#import <CoreLocation/CoreLocation.h>
